53e76417479bf3465ba4faced54871a5d5313fdd
Oleksii <alekspains8@gmail.com>
2025-11-17 20:47:28 +0100

    1

---
+++ kpi-api/Dto/CreateClassroomRequest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace kpi.API.Dto
+{
+    public class CreateClassroomRequest
+    {
+        [Required]
+        public string CourseName { get; set; } = string.Empty;
+
+        public string Section { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Room { get; set; } = string.Empty;
+
+        // Перший email у списку стане ВЛАСНИКОМ. Решта - запрошеними викладачами.
+        [Required]
+        public List<string> TeacherEmails { get; set; } = new();
+
+        public List<string> StudentEmails { get; set; } = new();
+    }
+}
